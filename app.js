33f77bb7958dde2afa2b386c33bc3845cd2e05f7
YustID <yudhaapriyadi00@gmail.com>
2024-03-19 16:05:39 +0700

    update fungsi

--- app.js
+++ app.js
@@ -1,27 +1,55 @@
-function createKeywords() {
-  var inputData = document.getElementById("input-data").value.split("\n");
-  var phraseChecked = document.getElementById("phrase-checkbox").checked;
-  var exactChecked = document.getElementById("exact-checkbox").checked;
-  var modifiedBroadChecked = document.getElementById("modifiedBroad-checkbox").checked;
+// function createKeywords() {
+//   var inputData = document.getElementById("input-data").value.split("\n");
+//   var phraseChecked = document.getElementById("phrase-checkbox").checked;
+//   var exactChecked = document.getElementById("exact-checkbox").checked;
+//   var modifiedBroadChecked = document.getElementById("modifiedBroad-checkbox").checked;
+//   var phraseOutput = "";
+//   var exactOutput = "";
+//   var modifiedBroadOutput = "";
+
+//   inputData.forEach(function (keyword) {
+//     if (phraseChecked) {
+//       phraseOutput += '"' + keyword.trim() + '"\n';
+//     }
+//     if (exactChecked) {
+//       exactOutput += "[" + keyword.trim() + "]\n";
+//     }
+//     if (modifiedBroadChecked) {
+//       modifiedBroadOutput += '"' + keyword.trim() + '" [' + keyword.trim() + "]\n";
+//     }
+//   });
+
+//   document.getElementById("phrase-output").value = phraseOutput;
+//   document.getElementById("exact-output").value = exactOutput;
+//   document.getElementById("modifiedBroad-output").value = modifiedBroadOutput;
+// }
+
+// function copyToClipboard(id) {
+//   var textarea = document.getElementById(id);
+//   textarea.select();
+//   document.execCommand("copy");
+//   alert("Copied to clipboard: " + textarea.value);
+// }
+
+function wrapKeywords(inputData) {
   var phraseOutput = "";
   var exactOutput = "";
   var modifiedBroadOutput = "";
 
   inputData.forEach(function (keyword) {
-    if (phraseChecked) {
-      phraseOutput += '"' + keyword.trim() + '"\n';
-    }
-    if (exactChecked) {
-      exactOutput += "[" + keyword.trim() + "]\n";
-    }
-    if (modifiedBroadChecked) {
-      modifiedBroadOutput += '"' + keyword.trim() + '" [' + keyword.trim() + "]\n";
-    }
+    phraseOutput += '"' + keyword.trim() + '"\n';
+    exactOutput += "[" + keyword.trim() + "]\n";
+    modifiedBroadOutput += '"' + keyword.trim() + '" [' + keyword.trim() + "]\n";
   });
 
   document.getElementById("phrase-output").value = phraseOutput;
   document.getElementById("exact-output").value = exactOutput;
   document.getElementById("modifiedBroad-output").value = modifiedBroadOutput;
+}
+
+function createKeywords() {
+  var inputData = document.getElementById("input-data").value.split("\n");
+  wrapKeywords(inputData);
 }
 
 function copyToClipboard(id) {
